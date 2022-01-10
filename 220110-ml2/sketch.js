0ff11219d1b4bf491747f303ed543a6a7a1a65bc
dersenn <67318888+dersenn@users.noreply.github.com>
2022-01-10 15:59:24 +0100

    some facemesh play. only works with specific window size.

--- 220110-ml2/sketch.js
+++ 220110-ml2/sketch.js
@@ -1,137 +1,187 @@
-// Canvas Vars
-const container = document.getElementById('p5-container')
-let canW = container.offsetWidth //canvas Width
-let canH = container.offsetHeight //canvas Height
-let canMax = Math.max(canW, canH) //longer canvas side
-let canMin = Math.min(canW, canH) //shorter canvas side
-
-// Sketch Vars
-let classifier
-let video
-
-let modelURL = "model/v1/model.json"
-let label
-let confidence
-
-let timer = 15
-let tabOpened = false
-
-let points = []
-
-function preload() {
-    classifier = ml5.imageClassifier(modelURL)
-    label = "Model loading…"
-}
 
 
-// p5 Setup
+let facemesh;
+let video;
+let predictions = [];
+
+/////////////////////////////////////////////////////////////// P5 SETUP
 function setup() {
-    let canvas = createCanvas(canW,canH)
-    canvas.parent(container)
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
 
-    video = createCapture(VIDEO)
-    video.hide()
+  facemesh = ml5.facemesh(video, modelReady);
 
-    textSize(32)
-    textAlign(CENTER, CENTER)
-    fill(255)
+  // This sets up an event that fills the global variable "predictions"
+  // with an array every time new predictions are made
+  facemesh.on("predict", results => {
+    predictions = results;
+  });
 
-    classifyVideo()
+  // Hide the video element, and just show the canvas
+  video.hide();
+
+  textSize(15)
+  textAlign(CENTER, CENTER)
 }
 
-// p5 Draw
+/////////////////////////////////////////////////////////////// P5 DRAW
 function draw() {
-    background(0)
-    image(video, 0, 0, canW, canW / 16 * 9)
-    text(label, width/2, height-64)
-    text(confidence, width/2, height-32)
+  background(0)
+  image(video, 0, 0, width, height);
+  filter(GRAY);
 
-    if (label == "Bottle") {
-        kurzePause()
-    } else if (label == "Lighter") {
-        streamTab()
-    } else if (label == "Pencil") {
-        zeichne()
-    } else {
-        tabOpened = false
-        clearPoints()
-        timer = 15
-    }
+  // bgnose()
+
+  colorMode(RGB, 255)
+
+  // We call function to draw all keypoints
+  raiseBrow()
+  drawSilhouette()
+  // drawKeypoints()
+  // drawRays()
+  // drawLeftEye()
+  mouthRays()
+
+  noStroke()
+  fill(255)
+  text("raise right eyebrow to change bg", width/2, 15)
+  text("open mouth for some rays", width/2, 33)
+
+  stroke(255)
+  strokeWeight(1)
+  noFill()
+  ellipse(width/2, height/2, height/2.5, height/2.5)
 }
 
-function classifyVideo() {
-    classifier.classify(video, gotResult)
+
+/////////////////////////////////////////////////////////////// FUNCTIONS
+
+function modelReady() {
+  console.log("Model ready!");
 }
 
-function gotResult(error, result) {
-    if (error) {
-        console.error(error)
-        return
-    }
-    classifyVideo()
+function raiseBrow() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const browPtLeft = predictions[i].annotations.leftEyebrowUpper[3]
+    const eyePtLeft = predictions[i].annotations.leftEyeUpper0[3]
 
-    label = result[0].label
-    confidence = nf(result[0].confidence * 100, 0, 2) + "%"
+    let dleft = dist(browPtLeft[0], browPtLeft[1], eyePtLeft[0], eyePtLeft[1])
+    let g = map(dleft, 15, 35, 0, 255)
+
+    fill(0, g, 0)
+    noStroke()
+    // blendMode(MULTIPLY)
+    rect(0, 0, width, height)
+
+    fill(255, 0, 0)
+    ellipse(browPtLeft[0], browPtLeft[1], 10, 10)
+    ellipse(eyePtLeft[0], eyePtLeft[1], 10, 10)
+  }
 }
 
-function kurzePause() {
-    tabOpened = false
-    clearPoints()
-    text(timer, width/2, height/2)
-    tint(255, 50)
-    if (frameCount % 30 == 0 && timer > 0) {
-        timer--
-    }
-    if (timer == 0) {
-        timer = ""
-        text("PAUSE VORBEI", width/2, height/2)
-        tint(255,255)
-    }
+function bgnose() {
+  for (let i = 0; i < predictions.length; i++) {
+    const nosetip = predictions[i].annotations.noseTip
+    let h = map(nosetip[0][0], 0, width, 0, 100)
+    let s = map(nosetip[0][1], 0, height, 0, 100)
+    colorMode(HSB, 100)
+    background(h, 100, s, 50)
+  }
 }
 
-function streamTab() {
-    clearPoints()
-    timer = 15
-    if(!tabOpened) {
-        window.open("https://www.youtube.com/watch?v=21X5lGlDOfg")
+function mouthRays() {
+  for (let i = 0; i < predictions.length; i++) {
+    const lipsLower = predictions[i].annotations.lipsLowerInner
+    const lipsUpper = predictions[i].annotations.lipsUpperInner
+    const lips = lipsLower.concat(lipsUpper)
+    const c = {
+      x: lipsLower[10][0] - (lipsLower[10][0]-lipsLower[0][0])/2,
+      y: lipsLower[5][1] - (lipsLower[5][1]-lipsUpper[5][1])/2
     }
-    tabOpened = true
+
+    let opening = dist(lipsLower[5][0], lipsLower[5][1], lipsUpper[5][0], lipsUpper[5][1])
+    if (opening > 10) {
+      for (let i in lips) {
+        let v = createVector(lips[i][0]-c.x, lips[i][1]-c.y)
+        stroke(255)
+        strokeWeight(1)
+        line(c.x, c.y, c.x + v.x * opening, c.y + v.y * opening)
+      }
+    }
+
+    // ellipse(c.x, c.y, 1, 1)
+  }
 }
 
-function zeichne() {
-    tabOpened = false
-    timer = 15
-    background(0, 0)
-    stroke(10)
-    strokeWeight(10)
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const keypoints = predictions[i].scaledMesh
 
-    beginShape(LINES)
-    for (let i in points) {
-        let singlePoint = points[i]
-        curveVertex(singlePoint.x, singlePoint.y)
+    // Draw facial keypoints.
+    for (let j = 0; j < keypoints.length; j += 1) {
+      const [x, y] = keypoints[j];
+
+      fill(0, 255, 0);
+      noStroke()
+      ellipse(x, y, 1, 1);
+    }
+  }
+}
+
+function drawSilhouette() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const silhouette = predictions[i].annotations.silhouette
+
+    noFill()
+    stroke(0, 0, 0)
+    strokeWeight(5)
+    beginShape(POINTS)
+    for (let i in silhouette) {
+        let singlePoint = silhouette[i]
+        curveVertex(singlePoint[0], singlePoint[1])
     }
     endShape()
+  }
 }
 
-function clearPoints() {
-    while(points.length > 0) {
-        points.pop()
+function drawLeftEye() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const upper = predictions[i].annotations.leftEyeUpper0
+    const lower = predictions[i].annotations.leftEyeLower0
+
+
+    noFill()
+    stroke(255, 0, 0)
+    strokeWeight(5)
+    beginShape(POINTS)
+    for (let i in upper) {
+        let singlePoint = upper[i]
+        curveVertex(singlePoint[0], singlePoint[1])
     }
-    noStroke()
+    endShape()
+    beginShape(POINTS)
+    for (let i in lower) {
+        let singlePoint = lower[i]
+        curveVertex(singlePoint[0], singlePoint[1])
+    }
+    endShape()
+  }
 }
 
-function mouseDragged() {
-    let singlePoint = {}
-    singlePoint.x = mouseX
-    singlePoint.y = mouseY
+function drawRays() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const keypoints = predictions[i].scaledMesh;
 
-    points.push(singlePoint)
+    // Draw facial keypoints.
+    for (let j = 0; j < keypoints.length; j += 1) {
+      const [x, y] = keypoints[j];
+
+      noFill()
+      stroke(0, 255, 0);
+      strokeWeight(1)
+      line(width/2, height/2, x, y)
+    }
+  }
 }
-
-
-
-
-
-
-
-// The End, My Friend.
