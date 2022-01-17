df30b8eb18fa0d676f83a8b8a07117889e0b35cd
dersenn <67318888+dersenn@users.noreply.github.com>
2022-01-17 14:25:07 +0100

    crazy eye.

--- 220117-faceApi-01/sketch.js
+++ 220117-faceApi-01/sketch.js
@@ -7,144 +7,150 @@ let canMin = Math.min(canW, canH) //shorter canvas side
 
 
 // Current Sketch Vars
-let modelRNN;
-let previous_pen = "down"
-let x, y;
-let strokePath;
+let faceapi;
+let video;
+let detections;
 
-let drawing;
-let clicked = false;
-let resetClicked = false;
-
-
-let buttons = ["b1", "b2", "b3", "b4", "b5"]
-let drawingArray = []
-
-let totalScore = 0;
+// by default all options are set to true
+const detectionOptions = {
+  withLandmarks: true,
+  withDescriptors: false,
+};
 
 function setup() {
-  cvs = createCanvas(canW, canH);
-  cvs.parent(container)
-  // console.log(models)
-  noStroke();
-  textAlign(CENTER, CENTER);
-  textFont("Helvetica");
-  textSize(32);
-  text("Errate die Skizze", width / 2, height / 2)
-  text("Drücke S zum Starten", width / 2, height / 2 + 100)
+  let can = createCanvas(canW, canH);
+  can.parent(container)
 
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i] = select("#" + buttons[i].toString());
-  }
-
+  // load up your video
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide(); // Hide the video element, and just show the canvas
+  faceapi = ml5.faceApi(video, detectionOptions, modelReady);
+  textAlign(RIGHT);
 }
 
-function modelReady() {
-  select("#status").html("modelRNN wurde geladen")
-  startDrawing();
-}
-
-function keyPressed() {
-  if (key == "s" | key == "S") {
-    if (!clicked) {
-      totalScore = 0;
-      select("#status").html("modelRNN am Laden")
-      drawing = models[Math.floor(Math.random() * models.length)];
-      modelRNN = ml5.sketchRNN(drawing, modelReady)
-      clicked = true;
-    }
-  }
-  if (key == "r" | key == "R") {
-    drawing = models[Math.floor(Math.random() * models.length)];
-    modelRNN = ml5.sketchRNN(drawing, modelReady)
-    resetClicked = true;
-  }
-}
-
-// Reset the drawing
-function startDrawing() {
-  resetButtons();
-  background(255);
-  strokeWeight(0)
-  fill(0)
-  text("Total Score", width - 200, 20);
-  text(totalScore, width - 50, 20)
-  // Start in the middle
-  x = width / 2;
-  y = height / 2;
-
-  // Generate the first stroke path
-  modelRNN.generate(gotStroke);
-}
+let counter = 0
 
 function draw() {
-  // If something new to draw
-  if (strokePath) {
-    // If the pen is down, draw a line
-    if (previous_pen == 'down') {
-      stroke(0);
-      strokeWeight(3.0);
-      line(x, y, x + strokePath.dx, y + strokePath.dy);
+  if (detections) {
+    if (counter > canH) {
+      counter = 0
     }
-    // Move the pen
-    x += strokePath.dx;
-    y += strokePath.dy;
-    // The pen state actually refers to the next stroke
-    previous_pen = strokePath.pen;
+    drawEyes(detections, counter)
+    counter += 5
+  } else {
+    // how to reset if detection has been lost?
+    counter = 0
+  }
+}
 
-    // If the drawing is complete
-    if (strokePath.pen !== 'end') {
-      strokePath = null;
-      modelRNN.generate(gotStroke);
+
+function modelReady() {
+  console.log("ready!");
+  // console.log(faceapi);
+  faceapi.detect(gotResults);
+}
+
+function gotResults(err, result) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  // console.log(result)
+  detections = result;
+
+  // background(220);
+  background(255);
+  image(video, 0, 0, width, height);
+  if (detections) {
+    if (detections.length > 0) {
+      // console.log(detections)
+      // drawBox(detections);
+      // drawLandmarks(detections);
+      // drawEyes(detections)
     }
   }
+  faceapi.detect(gotResults);
+}
 
-  if (totalScore > 20) {
-    clear();
-    strokeWeight(0);
-    fill(50, 180, 20);
-    text("YOU WON!", width / 2, height / 2)
-    text(" Drücke S zum Neustart", width / 2, height / 2 + 100);
-    clicked = false;
+function drawEyes(detections, counter) {
+  let c = counter
+
+  noStroke()
+
+  if (c % 10 == 0) {
+    blendMode(DIFFERENCE)
+  } else {
+    blendMode(OVERLAY)
+  }
+
+  for (let i = 0; i < detections.length; i++) {
+    let leftEye = detections[i].parts.leftEye;
+    let rightEye = detections[i].parts.rightEye;
+
+    fill(0, 255, 0)
+    ellipse(leftEye[1]._x, leftEye[1]._y, c+5, c+5)
+    fill(255, 0, 0)
+    ellipse(rightEye[1]._x, rightEye[1]._y, c+5, c+5)
+  }
+  blendMode(BLEND)
+}
+
+
+
+function drawBox(detections) {
+  for (let i = 0; i < detections.length; i += 1) {
+    const alignedRect = detections[i].alignedRect;
+    const x = alignedRect._box._x;
+    const y = alignedRect._box._y;
+    const boxWidth = alignedRect._box._width;
+    const boxHeight = alignedRect._box._height;
+
+    noFill();
+    stroke(0, 255, 0);
+    strokeWeight(2);
+    rect(x, y, boxWidth, boxHeight);
   }
 }
 
-// A new stroke path
-function gotStroke(err, s) {
-  strokePath = s;
-}
+function drawLandmarks(detections) {
+  noFill();
+  stroke(0, 255, 0);
+  strokeWeight(2);
 
+  for (let i = 0; i < detections.length; i += 1) {
+    const mouth = detections[i].parts.mouth;
+    const nose = detections[i].parts.nose;
+    const leftEye = detections[i].parts.leftEye;
+    const rightEye = detections[i].parts.rightEye;
+    const rightEyeBrow = detections[i].parts.rightEyeBrow;
+    const leftEyeBrow = detections[i].parts.leftEyeBrow;
+    const jawOutline = detections[i].parts.jawOutline;
 
-function resetButtons() {
-
-  drawingArray = []
-
-  for (let i = 0; i < buttons.length; i++) {
-    drawingArray.push(models[Math.floor(Math.random() * models.length)])
-  }
-
-  drawingArray.splice(Math.floor(Math.random() * drawingArray.length), 1, drawing)
-
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].html(drawingArray[i].toString());
-
-    buttons[i].mousePressed(function () {
-
-      if (buttons[i].html() == drawing) {
-        drawing = models[Math.floor(Math.random() * models.length)]
-        modelRNN = ml5.sketchRNN(drawing, modelReady);
-        totalScore += 10;
-      } else {
-        strokeWeight(0);
-        text("Game Over", width / 2, height / 2 - 100)
-        text("Drücke R zum Restarten", width / 2, height / 2)
-        fill(50, 180, 20);
-        text(`Die Antwort war : ${drawing}`, width / 2, height / 2 + 100);
-      }
-    })
+    drawPart(mouth, true);
+    drawPart(nose, false);
+    drawPart(leftEye, true);
+    drawPart(leftEyeBrow, false);
+    drawPart(rightEye, true);
+    drawPart(rightEyeBrow, false);
+    drawPart(jawOutline, false);
   }
 }
 
+function drawPart(feature, closed) {
+  beginShape();
+  for (let i = 0; i < feature.length; i += 1) {
+    const x = feature[i]._x;
+    const y = feature[i]._y;
+    vertex(x, y);
+  }
+
+  if (closed === true) {
+    endShape(CLOSE);
+  } else {
+    endShape();
+  }
+}
 
 
 
